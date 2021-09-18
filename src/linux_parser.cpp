579faa6c3cbaa93717c1b8c01cf225a6221813b9
lorenb <loren.barcus@gmail.com>
2021-09-18 10:10:29 -0400

    Update Processor::Utilization to use jiffies functions in linux parser.

--- src/linux_parser.cpp
+++ src/linux_parser.cpp
@@ -109,17 +109,83 @@ long LinuxParser::UpTime() {
 }
 
 // TODO: Read and return the number of jiffies for the system
-long LinuxParser::Jiffies() { return 0; }
+long LinuxParser::Jiffies() {
+    std::string line;
+    std::string key;
+    float cpu_user;
+    float cpu_nice;
+    float cpu_system;
+    float cpu_idle;
+    float cpu_iowait;
+    float cpu_irq;
+    float cpu_softirq;
+
+    std::ifstream stream(kProcDirectory + kStatFilename);
+    if (stream.is_open()) {
+      while(std::getline(stream, line)) {
+        if (line.rfind("cpu ", 0) == 0) {
+          std::istringstream linestream(line);
+          linestream >> key >> cpu_user >> cpu_nice >> cpu_system >> cpu_idle >> cpu_iowait >> cpu_irq >> cpu_softirq;
+        }
+      }
+    }
+  float result = cpu_user + cpu_nice + cpu_system + cpu_idle + cpu_iowait + cpu_irq + cpu_softirq;
+  return result;
+}
 
 // TODO: Read and return the number of active jiffies for a PID
 // REMOVE: [[maybe_unused]] once you define the function
 long LinuxParser::ActiveJiffies(int pid[[maybe_unused]]) { return 0; }
 
 // TODO: Read and return the number of active jiffies for the system
-long LinuxParser::ActiveJiffies() { return 0; }
+long LinuxParser::ActiveJiffies() {
+  std::string line;
+  std::string key;
+  float cpu_user;
+  float cpu_nice;
+  float cpu_system;
+  float cpu_idle;
+  float cpu_iowait;
+  float cpu_irq;
+  float cpu_softirq;
+
+  std::ifstream stream(kProcDirectory + kStatFilename);
+  if (stream.is_open()) {
+    while(std::getline(stream, line)) {
+      if (line.rfind("cpu ", 0) == 0) {
+        std::istringstream linestream(line);
+        linestream >> key >> cpu_user >> cpu_nice >> cpu_system >> cpu_idle >> cpu_iowait >> cpu_irq >> cpu_softirq;
+      }
+    }
+  }
+  float result = cpu_user + cpu_nice + cpu_system  + cpu_iowait + cpu_irq + cpu_softirq;
+  return result;
+}
 
 // TODO: Read and return the number of idle jiffies for the system
-long LinuxParser::IdleJiffies() { return 0; }
+long LinuxParser::IdleJiffies() {
+  std::string line;
+  std::string key;
+  float cpu_user;
+  float cpu_nice;
+  float cpu_system;
+  float cpu_idle;
+  float cpu_iowait;
+  float cpu_irq;
+  float cpu_softirq;
+
+  std::ifstream stream(kProcDirectory + kStatFilename);
+  if (stream.is_open()) {
+    while(std::getline(stream, line)) {
+      if (line.rfind("cpu ", 0) == 0) {
+        std::istringstream linestream(line);
+        linestream >> key >> cpu_user >> cpu_nice >> cpu_system >> cpu_idle >> cpu_iowait >> cpu_irq >> cpu_softirq;
+      }
+    }
+  }
+  float result = cpu_idle;
+  return result;
+}
 
 // TODO: Read and return CPU utilization
 vector<string> LinuxParser::CpuUtilization() { return {}; }
--- src/processor.cpp
+++ src/processor.cpp
@@ -3,30 +3,12 @@
 #include <string>
 
 #include "processor.h"
+#include "linux_parser.h"
 
 // TODO: Return the aggregate CPU utilization
 float Processor::Utilization() { 
-    std::string line;
-    std::string key;
-    float cpu_user;
-    float cpu_nice;
-    float cpu_system;
-    float cpu_idle;
-    float cpu_iowait;
-    float cpu_irq;
-    float cpu_softirq;
-
-    const std::string kProcDirectory{"/proc/"};
-    const std::string kStatFilename{"/stat"};
-    std::ifstream stream(kProcDirectory + kStatFilename);
-    if (stream.is_open()) {
-      while(std::getline(stream, line)) {
-        if (line.rfind("cpu ", 0) == 0) {
-          std::istringstream linestream(line);
-          linestream >> key >> cpu_user >> cpu_nice >> cpu_system >> cpu_idle >> cpu_iowait >> cpu_irq >> cpu_softirq;
-        }
-      }
-    }
-    float result = 1 -  cpu_idle  / ( cpu_user + cpu_nice + cpu_system + cpu_idle + cpu_iowait + cpu_irq + cpu_softirq );
+    float ActiveJiffies = LinuxParser::ActiveJiffies();
+    float Jiffies = LinuxParser::Jiffies();
+    float result =  ActiveJiffies / Jiffies;
     return result; 
 }
