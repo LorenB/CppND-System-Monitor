22fe5136e3e38ebac6ab2dafdc5faff6f44bd979
lorenb <loren.barcus@gmail.com>
2021-09-26 16:58:10 -0400

    Implement ElapsedTime.

--- src/format.cpp
+++ src/format.cpp
@@ -1,4 +1,5 @@
 #include <string>
+#include <time.h>
 
 #include "format.h"
 
@@ -7,5 +8,9 @@ using std::string;
 // TODO: Complete this helper function
 // INPUT: Long int measuring seconds
 // OUTPUT: HH:MM:SS
-// REMOVE: [[maybe_unused]] once you define the function
-string Format::ElapsedTime(long seconds[[maybe_unused]]) { return string(); }
+string Format::ElapsedTime(long seconds) {
+    time_t time_seconds(seconds);
+    tm *p = gmtime(&time_seconds);
+    string result = std::to_string(p->tm_hour) + ":" + std::to_string(p->tm_min) + ":" + std::to_string(p->tm_sec);
+    return result;
+}
