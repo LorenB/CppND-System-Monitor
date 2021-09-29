cfed1ceb8bad5431fc3116eaaa045160098f37de
lorenb <loren.barcus@gmail.com>
2021-09-29 00:09:36 -0400

    Fix compiler warning.

--- src/format.cpp
+++ src/format.cpp
@@ -11,7 +11,7 @@ using std::string;
 string Format::ElapsedTime(long seconds) {
     time_t time_seconds(seconds);
     tm *p = gmtime(&time_seconds);
-    char result[8];
+    char result[36];
     sprintf(result, "%02d:%02d:%02d", p->tm_hour, p->tm_min, p->tm_sec);
     return result;
 }
